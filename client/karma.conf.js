80ebd745833c029b16242ba88da4bb95c3e2214f
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2021-08-19 00:36:51 +0700

    fix

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-health-care-demo'),
+      dir: require('path').join(__dirname, './coverage/ta-sys-online'),
       subdir: '.',
       reporters: [
         { type: 'html' },
