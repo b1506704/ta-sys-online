9ba58c430980752b4a56f1871a63a527b8c51514
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2021-10-08 22:04:13 +0700

    paypal api

--- client/src/app/pages/visitor/course-list/course-detail/course-detail.component.ts
+++ client/src/app/pages/visitor/course-list/course-detail/course-detail.component.ts
@@ -4,6 +4,8 @@ import { CourseStore } from '../../../../shared/services/course/course-store.ser
 import { File } from 'src/app/shared/models/file';
 import { FileHttpService } from 'src/app/shared/services/file/file-http.service';
 import { FileStore } from 'src/app/shared/services/file/file-store.service';
+import { ICreateOrderRequest, IPayPalConfig } from 'ngx-paypal';
+import { BillStore } from 'src/app/shared/services/bill/bill-store.service';
 @Component({
   selector: 'app-course-detail',
   templateUrl: 'course-detail.component.html',
@@ -24,11 +26,18 @@ export class CourseDetailComponent implements OnInit, OnDestroy, OnChanges {
     fileType: '',
     url: '../../../../assets/imgs/profile.png',
   };
-  constructor(private courseStore: CourseStore, private fileStore: FileStore) {}
+  payPalConfig?: IPayPalConfig;
+  showSuccess: boolean;
+  constructor(
+    private courseStore: CourseStore,
+    private fileStore: FileStore,
+    private billStore: BillStore
+  ) {}
 
   courseDataListener() {
     return this.courseStore.$courseInstance.subscribe((data: any) => {
       this.courseData = data;
+      this.initConfig();
     });
   }
 
@@ -52,15 +61,90 @@ export class CourseDetailComponent implements OnInit, OnDestroy, OnChanges {
       fileType: '',
       url: '../../../../assets/imgs/profile.png',
     };
-    setTimeout(() => {
-      this.getUserMediaData(this.courseID);
-      this.courseStore.getCourse(this.courseID).then(() => {
-        this.courseDataListener();
-      });
-    }, 100);
+    // setTimeout(() => {
+    // this.getUserMediaData(this.courseID);
+    this.courseStore.getCourse(this.courseID).then(() => {
+      this.courseDataListener();
+    });
+    // }, 100);
   }
 
-  ngOnInit(): void {}
+  initConfig(): void {
+    this.payPalConfig = {
+      currency: 'USD',
+      clientId:
+        'ASfFvJbPpYwF_4PlZ7WMC7tJ6-0rlQtvzOpz31TJ21g2BlXQ2oNfMHuiUKNy7UT7ofkynkQl5-k8VkCf',
+      createOrderOnClient: (data: any) =>
+        <ICreateOrderRequest>{
+          intent: 'CAPTURE',
+          purchase_units: [
+            {
+              amount: {
+                currency_code: 'USD',
+                value: this.courseData.cost.toString(),
+                breakdown: {
+                  item_total: {
+                    currency_code: 'USD',
+                    value: this.courseData.cost.toString(),
+                  },
+                },
+              },
+              items: [
+                {
+                  name: this.courseData.name,
+                  quantity: '1',
+                  category: 'DIGITAL_GOODS',
+                  unit_amount: {
+                    currency_code: 'USD',
+                    value: this.courseData.cost.toString(),
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      advanced: {
+        commit: 'false',
+      },
+      style: {
+        label: 'paypal',
+        layout: 'vertical',
+      },
+      onApprove: (data, actions) => {
+        console.log(
+          'onApprove - transaction was approved, but not authorized',
+          data,
+          actions
+        );
+        actions.order.get().then((details: any) => {
+          console.log(
+            'onApprove - you can get full order details inside onApprove: ',
+            details
+          );
+        });
+      },
+      onClientAuthorization: (data) => {
+        console.log(
+          'onClientAuthorization - you should probably inform your server about completed transaction at this point',
+          data
+        );
+        this.showSuccess = true;
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel', data, actions);
+      },
+      onError: (err) => {
+        console.log('OnError', err);
+      },
+      onClick: (data, actions) => {
+        console.log('onClick', data, actions);
+      },
+    };
+  }
+
+  ngOnInit(): void {
+    // this.initConfig();
+  }
 
   ngOnChanges(): void {
     this.renderSourceData();
